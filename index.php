ea03f88c307d8bda7166de8c305ac79abd2aa37b
Torsti Tapiola <torsti.tapiola@edu.sasky.fi>
2021-12-15 16:53:19 +0200

    tyylitiedosto lisätty

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <head>
     <meta charset="UTF-8">
     <title>Perhekalenteri</title>
+    <link href="style.php" rel="stylesheet">
   </head>
   <body>
     <header>
--- style.php
+++ style.php
@@ -20,6 +20,7 @@ body {
   font-family: 'Lato', sans-serif;
   margin: 0;
   padding: 0;
+  color: #8B008B;
 }
 
 h1 {
